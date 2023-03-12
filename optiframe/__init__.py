a9af97197a2cf841fe5cad32cff2acee2d34db97
Tim Jentzsch <commits@timjen.net>
2023-03-12 18:11:30 +0100

    Add additional re-exports

--- optiframe/__init__.py
+++ optiframe/__init__.py
@@ -1,2 +1,4 @@
-from optiframe.workflow_engine import Task
+from optiframe.workflow_engine import Task, StepData
 from optiframe.framework import Optimizer
+from optiframe.framework.errors import SolveError, SolveErrorReason
+from optiframe.framework.tasks import SolutionCost
