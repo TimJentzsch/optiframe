563100fef93b8b0c6304603b12f22078f63d29c9
Tim Jentzsch <commits@timjen.net>
2023-03-22 11:58:26 +0100

    Declare reexports to surpress lint warnings

--- optiframe/__init__.py
+++ optiframe/__init__.py
@@ -2,3 +2,5 @@ from optiframe.workflow_engine import Task, StepData
 from optiframe.framework import Optimizer
 from optiframe.framework.errors import InfeasibleError
 from optiframe.framework.tasks import SolutionObjValue
+
+__all__ = ["Task", "StepData", "Optimizer", "InfeasibleError", "SolutionObjValue"]
--- optiframe/workflow_engine/__init__.py
+++ optiframe/workflow_engine/__init__.py
@@ -1,3 +1,5 @@
 from .task import Task
 from .step import Step, InitializedStep, StepData
 from .workflow import Workflow
+
+__all__ = ["Task", "Step", "InitializedStep", "StepData", "Workflow"]
