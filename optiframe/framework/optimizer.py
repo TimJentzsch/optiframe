f7b32d2cca09dd39b6d695fcedb47a1cfcc3694a
Tim Jentzsch <commits@timjen.net>
2023-04-01 18:42:08 +0200

    Add a test for the conflict package

--- optiframe/framework/optimizer.py
+++ optiframe/framework/optimizer.py
@@ -119,7 +119,7 @@ class BuiltOptimizer:
         self.workflow.execute_step(2)
         return self.workflow.execute_step(3)
 
-    def print_mip_and_solve(self, solver: Optional[Any] = None):
+    def print_mip_and_solve(self, solver: Optional[Any] = None) -> StepData:
         """Print the description of the MIP and solve it."""
         print(self.get_lp_string())
         return self.solve(solver)
--- tests/test_knapsack.py
+++ tests/test_knapsack.py
@@ -1,12 +1,18 @@
 from pulp import LpMaximize
 from pytest import approx
 
+from examples.knapsack import conflict_package, ConflictData
 from examples.knapsack.base_package import BaseData, base_package
 from examples.knapsack.base_package import SolutionData
 from optiframe import SolutionObjValue, Optimizer
 
 
 base_optimizer = Optimizer("knapsack_base", sense=LpMaximize).add_package(base_package)
+conflict_optimizer = (
+    Optimizer("knapsack_conflict", sense=LpMaximize)
+    .add_package(base_package)
+    .add_package(conflict_package)
+)
 
 
 def test_one_fitting_item() -> None:
@@ -44,3 +50,32 @@ def test_two_items_one_fits() -> None:
 
     assert solution[SolutionObjValue].objective_value == approx(2.0)
     assert solution[SolutionData].packed_items == ["banana"]
+
+
+def test_conflict() -> None:
+    """
+    There are three items.
+    The first two fit in the knapsack together and would yield the most profit.
+    The third item fills the whole knapsack and is worth less than one and two,
+    but more than only one of them.
+    The first two items are conflicting, so the solution is to pack the third item.
+    """
+    solution = (
+        conflict_optimizer.initialize(
+            BaseData(
+                items=["apple", "banana", "kiwi"],
+                profits={"apple": 2.0, "banana": 2.0, "kiwi": 3.0},
+                weights={"apple": 1.0, "banana": 1.0, "kiwi": 2.0},
+                max_weight=2.0,
+            ),
+            ConflictData(
+                conflicts=[("apple", "banana")],
+            )
+        )
+        .validate()
+        .build_mip()
+        .print_mip_and_solve()
+    )
+
+    assert solution[SolutionObjValue].objective_value == approx(3.0)
+    assert solution[SolutionData].packed_items == ["kiwi"]
